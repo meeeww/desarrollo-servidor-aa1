a40cda0c6ce94300cdba7c399d20c84cddee89c8
meeeww <thezasm03@gmail.com>
2023-11-27 18:23:38 +0100

    Nuevas entidades y repositorios

---
+++ BankAPI.Model/DetallePedido.cs
@@ -0,0 +1,11 @@
+﻿namespace BankAPI.Model
+{
+    public class DetallePedido
+    {
+        public int ID_DetallePedido { get; set; }
+        public int ID_Pedido { get; set; }
+        public int ID_Producto { get; set; }
+        public int Cantidad { get; set; }
+        public decimal Subtotal { get; set; }
+    }
+}
