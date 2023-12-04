965f1ff5e7d05da798d485cbb73c4c0246301728
Rakions <ogmohammed2004@hotmail.com>
2023-12-04 20:45:31 +0100

    controllers terminados

--- BankAPI/Controllers/DetallePedidosController.cs
+++ BankAPI/Controllers/DetallePedidosController.cs
@@ -20,5 +20,47 @@ namespace BankAPI.Controllers
         {
             return Ok(await _detallePedidoRepository.GetDetallesPedido());
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetDetallePedidoById(int id)
+        {
+            return Ok(await _detallePedidoRepository.GetDetallePedidoById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertDetallePedido([FromBody] DetallePedido detallePedido)
+        {
+            if (detallePedido == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _detallePedidoRepository.InsertDetallePedido(detallePedido);
+
+            return Created("creado", created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateDetallePedido([FromBody] DetallePedido detallePedido)
+        {
+            if (detallePedido == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _detallePedidoRepository.UpdateDetallePedido(detallePedido);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDetallePedido(int id)
+        {
+            await _detallePedidoRepository.DeleteDetallePedido(new DetallePedido() { ID_DetallePedido = id });
+
+            return NoContent();
+        }
     }
 }
--- BankAPI/Controllers/EmpleadosController.cs
+++ BankAPI/Controllers/EmpleadosController.cs
@@ -20,5 +20,47 @@ namespace BankAPI.Controllers
         {
             return Ok(await _empleadoRepository.GetEmpleados());
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmpleadoById(int id)
+        {
+            return Ok(await _empleadoRepository.GetEmpleadoById(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> InsertEmpleado([FromBody] Empleado empleado)
+        {
+            if (empleado == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _empleadoRepository.InsertEmpleado(empleado);
+
+            return Created("creado", created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateEmpleado([FromBody] Empleado empleado)
+        {
+            if (empleado == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _empleadoRepository.UpdateEmpleado(empleado);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmpleado(int id)
+        {
+            await _empleadoRepository.DeleteEmpleado(new Empleado() { ID_Empleado = id });
+
+            return NoContent();
+        }
     }
 }
