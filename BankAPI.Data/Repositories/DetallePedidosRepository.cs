72b485de44bb90d0373d86aa39ba4caf06685d0d
Rakions <ogmohammed2004@hotmail.com>
2023-12-04 22:29:23 +0100

    fixed controller

--- BankAPI.Data/Repositories/DetallePedidosRepository.cs
+++ BankAPI.Data/Repositories/DetallePedidosRepository.cs
@@ -46,7 +46,7 @@ public class DetallePedidosRepository : IDetallePedidosRepository
     public async Task<bool> InsertDetallePedido(DetallePedido detallePedido)
     {
         var db = dbConnection();
-        var sql = @"INSERT INTO DetallePedidos (ID_Pedido , ID_Producto , Cantidad , Subtotal) VALUES (@ID_Pedido , @ID_Producto , @Cantidad , @Subtotal)";
+        var sql = @"INSERT INTO DetallePedidos (ID_Pedido , ID_Producto , Cantidad , Subtotal, FechaCreacion , FechaModificacion) VALUES (@ID_Pedido , @ID_Producto , @Cantidad , @Subtotal , @FechaModificacion, @FechaModificacion)";
         var result = await db.ExecuteAsync(sql, new { detallePedido.ID_Pedido, detallePedido.ID_Producto, detallePedido.Cantidad, detallePedido.Subtotal });
 
 
